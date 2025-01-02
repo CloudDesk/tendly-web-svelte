0bece2070190344806a607f56f659bb941c9e6ac
sachi-csod <skumaragurubaran@csod.com>
2025-01-02 14:55:28 +0530

    clean swipe pages

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,14 +1,13 @@
 <script lang="ts">
   import '../app.css';
   import Sidebar from '$lib/components/common/Sidebar.svelte';
-  import { auth } from '$lib/stores/auth';
   import { page } from '$app/stores';
 
-  $: isLoginPage = $page.url.pathname === '/login';
+  $: isPublicPage = ['/login', '/swipe', '/training-swipe'].includes($page.url.pathname);
 </script>
 
 <div class="min-h-screen bg-base-200" data-theme="tendlyPro">
-  {#if !isLoginPage}
+  {#if !isPublicPage}
     <div class="flex">
       <Sidebar />
       <div class="flex-1 ml-64">
