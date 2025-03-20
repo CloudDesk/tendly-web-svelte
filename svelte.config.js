5576ff1c634aaf02f171f5ec642e986bf33c55bf
suresh-on-cloud <sureshkumar@clouddesk.ae>
2025-03-20 12:17:19 +0530

    build

--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,21 @@
-import adapter from '@sveltejs/adapter-auto';
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import adapter from "@sveltejs/adapter-static";
+import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
-    adapter: adapter(),
+    adapter: adapter({
+      prerender: { default: false },
+      pages: "build", // Output HTML files here
+      assets: "build", // Output assets (JS, CSS) here
+      fallback: "index.html", // For SPA routing
+      precompress: false,
+    }),
     alias: {
-      $lib: './src/lib'
-    }
+      $lib: "./src/lib",
+    },
   },
-  preprocess: vitePreprocess()
+  preprocess: vitePreprocess(),
 };
 
-export default config; 
+export default config;
